71c81b582c526c33508a2f188e332a9e21608624
Ashraf Tantawy <36851667+Ashraf-Tantawy@users.noreply.github.com>
2022-12-31 21:58:40 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,24 +1,62 @@
-echo "Running tests..."
-echo
+#!/bin/bash
 
-output=$(./a.out)
-expected_output="Hello, World!"
+numTests=10
+numCountMax=20
+numCount=''
+declare -a numArr
+sum=''
+avg=''
+minNum=''
+maxNum=''
+score=0
 
-if [ $? -eq 0 ] ; then
-  echo "Pass: Program exited zero"
-else
-  echo "Fail: Program did not exit zero"
-  exit 1
-fi
+for test in {1..$numTests}; do
+	# Generate numbers
+	echo Test $test ..................
+	numCount=$(($RANDOM%$numCountMax))
+	for idx in {1..$numCount}; do
+		numArr[$idx]=$(($RANDOM%1000))
+	done
+	
+	# Calculate average
+	sum=0
+	for idx in {1..$numCount}; do
+		sum = $(($sum + $numArr[$idx]))
+	avg=$(($sum/$numCount))
+	
+	# Calculate minimum
+	minNum=$numArr[0]
+	for idx in {1..$numCount}; do
+		if [ "$numArr[$idx]" < "$minNum" ]; then
+			minNum=$(($numArr[$idx]))
+		fi
+	done
+	
+	# Calculate maximum
+	maxNum=$numArr[0]
+	for idx in {1..$numCount}; do
+		if [ "$numArr[$idx]" > "$maxNum" ]; then
+			maxNum=$(($numArr[$idx]))
+		fi
+	done
+	
+	output=$($(echo ./a.out ${numArr[a]}))
+	expected_output="$avg $minNum $maxNum"
+	
+	if [ $? -eq 0 ]; then
+		echo "Program executed successfully"
+	else
+		echo "Program failed to execute"
+		exit 1
+	fi
+	
+	if [ "$output" == "$expected_output" ]; then
+		echo "Passed test $test"
+		score=$(($score + 1))
+	else
+		echo "Failed test $test, expected output: '$expected_output', your output: '$output'"
+	fi
+	
+done
 
-if [ "$output" == "$expected_output" ] ; then
-  echo "Pass: Output is correct"
-else
-  echo "Expected '$expected_output' but got: $output"
-  exit 1
-fi
-
-echo
-echo "All tests passed."
-
-exit 0
+echo "Tests completed"

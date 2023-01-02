bf6ab3052deaa03842bc8ab2b251d2fc80ea6726
Ashraf Tantawy <36851667+Ashraf-Tantawy@users.noreply.github.com>
2023-01-02 12:22:21 +0000

    Add files via upload

--- multithreadsol.c
+++ multithreadsol.c
@@ -2,42 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float avg;
-float min;
+int avg;
+int min;
+int max;
 
 void *avrg(void *param);
 void *minn(void *param);
-
-typedef struct tARGS
-{
-    int argc;
-    char** argv;
-} ARGS;
+void *maxn(void* param);
 
 int main(int argc, char* argv[])
 {
- 
-   ARGS args;
-   args.argc = argc;
-   args.argv = argv;
-   
-   pthread_t tid1;
-   pthread_attr_t attr1;
+    pthread_t tid1;
+    pthread_attr_t attr1;
 
     pthread_t tid2;
     pthread_attr_t attr2;
 
+    pthread_t tid3;
+    pthread_attr_t attr3;
+
+
     pthread_attr_init(&attr1);
     pthread_attr_init(&attr2);
+    pthread_attr_init(&attr3);
 
     pthread_create(&tid1, &attr1, avrg, argv);
     pthread_create(&tid2, &attr2, minn, argv);
+    pthread_create(&tid3, &attr3, maxn, argv);
 
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
+    pthread_join(tid3, NULL);
 
-    printf("average = %f\n", avg);
-    printf("min is %f\n", min);
+    printf("%d %d %d", avg, min, max);
 }
 
 void *avrg(void *param)
@@ -67,6 +64,23 @@ void *minn(void *param)
         if(num < min)
         {
             min = num;
+        }
+        *args++;
+    }
+}
+
+void *maxn(void *param)
+{
+    char** args = (char**) param;
+    *args++;
+    max = atoi(*args);
+    float num;
+    while(*args)
+    {
+        num = atoi(*args);
+        if(num > max)
+        {
+            max = num;
         }
         *args++;
     }

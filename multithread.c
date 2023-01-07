dfc210c05390d26804e8172558a1e991d8a64988
Ashraf Tantawy <36851667+Ashraf-Tantawy@users.noreply.github.com>
2023-01-07 19:39:21 +0000

    test

--- multithread.c
+++ multithread.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Variables that hold the final results, integer data type for simplicity
 int avgNum;
 int minNum;
 int maxNum;
@@ -18,6 +19,7 @@ int main(int argc, char* argv[])
 
 /* Create thread attributes */
 
+
 /* Initialize thread attributes */
 
 
@@ -26,14 +28,16 @@ int main(int argc, char* argv[])
 
 /* Wait for threads to exit */
 
+
 printf("%d %d %d", avgNum, minNum, maxNum);
 
 
 void *avgfunc(void *param)
 {
+    // The following two lines help you to access the arguments (list of numbers) passed to the program
     char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
     *args++;                        // increment the pointer to point to the first number passed
- /* Write the code to calculate the average */
+ /* Write the code to calculate the average value */
  // To get a number from args array, use atoi(*args)
  // to increment the pointer to point to the next number: *args++
  
@@ -42,19 +46,22 @@ void *avgfunc(void *param)
 
 void *minfunc(void *param)
 {
+    // The following two lines help you to access the arguments (list of numbers) passed to the program
     char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
     *args++;                        // increment the pointer to point to the first number passed
  
- /* Write the code to calculate the average */
+ /* Write the code to calculate the minimum value */
  // To get a number from args array, use atoi(*args)
  // to increment the pointer to point to the next number: *args++
 }
 
 void *maxfunc(void *param)
 {
+    // The following two lines help you to access the arguments (list of numbers) passed to the program
     char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
     *args++;                        // increment the pointer to point to the first number passed
- /* Write the code to calculate the average */
+ 
+ /* Write the code to calculate the maximum value */
  // To get a number from args array, use atoi(*args)
  // to increment the pointer to point to the next number: *args++
 

915aff9ee2374a6db7f43e618b5fd84e9e5c4f97
Ashraf Tantawy <36851667+Ashraf-Tantawy@users.noreply.github.com>
2023-01-07 20:20:55 +0000

    Final

--- multithread.c
+++ multithread.c
@@ -29,27 +29,6 @@ int main(int argc, char* argv[])
 
 /* Wait for threads to exit */
 
-pthread_t tid1;
-    pthread_attr_t attr1;
-
-    pthread_t tid2;
-    pthread_attr_t attr2;
-
-    pthread_t tid3;
-    pthread_attr_t attr3;
-
-
-    pthread_attr_init(&attr1);
-    pthread_attr_init(&attr2);
-    pthread_attr_init(&attr3);
-
-    pthread_create(&tid1, &attr1, avgfunc, argv);
-    pthread_create(&tid2, &attr2, minfunc, argv);
-    pthread_create(&tid3, &attr3, maxfunc, argv);
-
-    pthread_join(tid1, NULL);
-    pthread_join(tid2, NULL);
-    pthread_join(tid3, NULL);
 
 
 printf("%d %d %d", avgNum, minNum, maxNum);
@@ -60,18 +39,13 @@ void *avgfunc(void *param)
     // The following two lines help you to access the arguments (list of numbers) passed to the program
     char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
     *args++;                        // increment the pointer to point to the first number passed
- /* Write the code to calculate the average value and store it in avgNum variable */
- // To get a number from args array, use atoi(*args)
- // to increment the pointer to point to the next number: *args++
-    int sum = 0;
-    int i = 0;
-    while(*args)
-    {
-        sum += atoi(*args);
-        *args++;
-        i++;
-    }
-    avgNum = sum/i;
+
+    // To get a number from args array, use atoi(*args)
+    // to increment the pointer to point to the next number: *args++
+ 
+    /* Write the code to calculate the average value and store it in avgNum variable */
+ 
+
 
 }
 
@@ -81,20 +55,12 @@ void *minfunc(void *param)
     char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
     *args++;                        // increment the pointer to point to the first number passed
  
- /* Write the code to calculate the minimum value and store it in minNum variable */
- // To get a number from args array, use atoi(*args)
- // to increment the pointer to point to the next number: *args++
-    minNum = atoi(*args);
-    int num;
-    while(*args)
-    {
-        num = atoi(*args);
-        if(num < minNum)
-        {
-            minNum = num;
-        }
-        *args++;
-    }
+    // To get a number from args array, use atoi(*args)
+    // to increment the pointer to point to the next number: *args++
+    
+    /* Write the code to calculate the minimum value and store it in minNum variable */
+ 
+ 
 
 }
 
@@ -104,19 +70,11 @@ void *maxfunc(void *param)
     char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
     *args++;                        // increment the pointer to point to the first number passed
  
- /* Write the code to calculate the maximum value and store it in maxNum variable */
- // To get a number from args array, use atoi(*args)
- // to increment the pointer to point to the next number: *args++
-    maxNum = atoi(*args);
-    int num;
-    while(*args)
-    {
-        num = atoi(*args);
-        if(num > maxNum)
-        {
-            maxNum = num;
-        }
-        *args++;
-    }
+    // To get a number from args array, use atoi(*args)
+    // to increment the pointer to point to the next number: *args++
+    
+    /* Write the code to calculate the maximum value and store it in maxNum variable */
+ 
+ 
 
 }

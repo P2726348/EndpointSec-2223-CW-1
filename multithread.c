5eaa7ce5b5d60229a2aa8a5b7562ca46c52c7e24
P2726348 <123334536+P2726348@users.noreply.github.com>
2023-02-03 01:37:47 +0000

    Update multithread.c

--- multithread.c
+++ multithread.c
@@ -1,80 +1,86 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 // Variables that hold the final results, integer data type for simplicity
 int avgNum;
 int minNum;
 int maxNum;
 
-// Functions' declaration. Definition is at the bottom
+// The function decleration. 
 void *avgfunc(void *param);
 void *minfunc(void *param);
 void *maxfunc(void *param);
 
+//Main function handling pthreads
 int main(int argc, char* argv[])
 {
- 
- /* Create thread IDs */
+//3 pthreads assigned for each calculation 
+pthread_t avg, min, max;
+//the pthread attribution 
+pthread_attr_t attr;
+//execute the pthread attribution 
+pthread_attr_init(&attr);
 
+// 3 pthreads will be created with each function passed to the pthreads for execution.
+pthread_create(&avg, &attr, *avgfunc, argv);
+pthread_create(&min, &attr, *minfunc, argv);
+pthread_create(&max, &attr, *maxfunc, argv);
 
-/* Create thread attributes */
+//wait till pthread execution finished
+pthread_join(avg, NULL);
+pthread_join(min, NULL);
+pthread_join(max, NULL);
 
-
-/* Initialize thread attributes */
-
-
-/* Create threads */
-
-
-/* Wait for threads to exit */
-
-
-
+//print values
 printf("%d %d %d", avgNum, minNum, maxNum);
 }
-
+//the functions determines the average of the user provided numbers
 void *avgfunc(void *param)
 {
-    // The following two lines help you to access the arguments (list of numbers) passed to the program
-    char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
-    args++;                        // increment the pointer to point to the first number passed
-
-    // To get a number from args array, use atoi(*args)
-    // to increment the pointer to point to the next number: *args++
  
-    /* Write the code to calculate the average value and store it in avgNum variable */
- 
+    char** args = (char**) param; // args is a pointer to array of pointers to main func arguments argv
+    args++; // increment the pointer to point to the first number passed
 
-
+    int sum = 0; //initialise sum
+    int num_of_args = 0; //initialise num of args to 0
+    while (*args != NULL) //add integer number from user provided input within a loop.
+    {
+        sum += atoi(*args); //swap strings to integer
+        num_of_args++; //increments pointer
+        args++; //increments pointer
+    }
+    avgNum = sum/num_of_args; 
 }
-
+//the functions determines the minimum of the user provided numbers
 void *minfunc(void *param)
 {
-    // The following two lines help you to access the arguments (list of numbers) passed to the program
-    char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
-    args++;                        // increment the pointer to point to the first number passed
+    char** args = (char**) param;
+    args++;  //increments pointer                  
  
-    // To get a number from args array, use atoi(*args)
-    // to increment the pointer to point to the next number: *args++
-    
-    /* Write the code to calculate the minimum value and store it in minNum variable */
- 
- 
-
+int min = INT_MAX; //the int data type can stay by setting variable max to the minimum value
+while (*args != NULL) //add integer number from user provided input within a loop.
+{
+    int temp = atoi(*args);  //converts string pointed by args to iinteger, storing to temp.
+    if (temp < min) min = temp; //if temp is lower value than min, min is updated to temp.
+    args++; //increments pointer
 }
-
+minNum = min;
+}
+//the functions determines the maximum of the user provided numbers
 void *maxfunc(void *param)
 {
-    // The following two lines help you to access the arguments (list of numbers) passed to the program
-    char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
-    args++;                        // increment the pointer to point to the first number passed
  
-    // To get a number from args array, use atoi(*args)
-    // to increment the pointer to point to the next number: *args++
-    
-    /* Write the code to calculate the maximum value and store it in maxNum variable */
+    char** args = (char**) param; 
+    args++; //increments pointer
  
- 
-
+int max = INT_MIN; //the int data type can stay by setting variable min to the maximum value
+while (*args != NULL) //add integer number from user provided input within a loop.
+{
+    int temp = atoi(*args); //converts string pointed by args to iinteger, storing to temp.
+    if (temp > max) max = temp; //if temp is higher value than max, max is updated to temp.
+    args++; //increments pointer
+}
+maxNum = max;
 }
